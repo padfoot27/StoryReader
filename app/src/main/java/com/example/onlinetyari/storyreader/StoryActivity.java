4b56bceabfdce2e0b01a53468022f21c391e85b6
Siddharth Verma <siddharthv93@gmail.com>
2016-06-06 10:17:03 +0530

    Add pagination to text view

--- app/src/main/java/com/example/onlinetyari/storyreader/StoryActivity.java
+++ app/src/main/java/com/example/onlinetyari/storyreader/StoryActivity.java
@@ -1,19 +1,31 @@
 package com.example.onlinetyari.storyreader;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.text.Spanned;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewTreeObserver;
+import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class StoryActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
 
     public TextView textView;
+
+    public Pagination mPagination;
+    public CharSequence mText;
+    public int mCurrentIndex = 0;
+    public Button backButton;
+    public Button forwardButton;
     public SeekBar seekBar;
 
     @Override
@@ -33,14 +45,72 @@ public class StoryActivity extends AppCompatActivity implements SeekBar.OnSeekBa
             }
         });
 
-        seekBar = (SeekBar) findViewById(R.id.seekbar);
         textView = (TextView) findViewById(R.id.textView);
 
         assert textView != null;
-        textView.setTextSize(seekBar.getProgress());
 
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 100; i++) {
+            sb.append("This text helps to create some bulk content. " + i + " \n");
+        }
+        String book_content = sb.toString();
+
+        Spanned htmlString = Html.fromHtml(book_content);
+        mText = TextUtils.concat(htmlString);
+
+        textView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                // Removing layout listener to avoid multiple calls
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+                    textView.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                } else {
+                    textView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                }
+                mPagination = new Pagination(mText,
+                        textView.getWidth(),
+                        textView.getHeight(),
+                        textView.getPaint(),
+                        textView.getLineSpacingMultiplier(),
+                        textView.getLineSpacingExtra(),
+                        textView.getIncludeFontPadding());
+                update();
+            }
+        });
+
+        backButton = (Button) findViewById(R.id.back_btn);
+        forwardButton = (Button) findViewById(R.id.forward_btn);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mCurrentIndex > 0) {
+                    mCurrentIndex--;
+                    update();
+                }
+            }
+        });
+
+        forwardButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mCurrentIndex < mPagination.size()) {
+                    mCurrentIndex++;
+                    update();
+                }
+            }
+        });
+
+        seekBar = (SeekBar) findViewById(R.id.seekbar);
         assert seekBar != null;
+        seekBar.setProgress((int) textView.getTextSize());
         seekBar.setOnSeekBarChangeListener(this);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                seekBar.setVisibility(View.VISIBLE);
+            }
+        });
     }
 
     @Override
@@ -77,6 +147,12 @@ public class StoryActivity extends AppCompatActivity implements SeekBar.OnSeekBa
 
     @Override
     public void onStopTrackingTouch(SeekBar seekBar) {
-
+        seekBar.setVisibility(View.GONE);
     }
+
+    private void update() {
+        final CharSequence text = mPagination.get(mCurrentIndex);
+        if (text != null) textView.setText(text);
+    }
+
 }
