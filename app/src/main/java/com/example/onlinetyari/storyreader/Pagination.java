517bf04a40afaf962ccdfe8cd3474e4b4011422c
Siddharth Verma <siddharthv93@gmail.com>
2016-06-07 01:28:37 +0530

    Add pagination

--- app/src/main/java/com/example/onlinetyari/storyreader/Pagination.java
+++ app/src/main/java/com/example/onlinetyari/storyreader/Pagination.java
@@ -1,10 +1,13 @@
 package com.example.onlinetyari.storyreader;
 
+import android.text.DynamicLayout;
 import android.text.Layout;
 import android.text.StaticLayout;
 import android.text.TextPaint;
+import android.util.Log;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -18,9 +21,10 @@ public class Pagination {
     private final float mSpacingAdd;
     private final CharSequence mText;
     private final TextPaint mPaint;
-    private final List<CharSequence> mPages;
+    public final List<CharSequence> mPages;
+    private final boolean mReverse;
 
-    public Pagination(CharSequence text, int pageW, int pageH, TextPaint paint, float spacingMult, float spacingAdd, boolean inclidePad) {
+    public Pagination(CharSequence text, int pageW, int pageH, TextPaint paint, float spacingMult, float spacingAdd, boolean inclidePad, boolean reverse) {
         this.mText = text;
         this.mWidth = pageW;
         this.mHeight = pageH;
@@ -29,8 +33,12 @@ public class Pagination {
         this.mSpacingAdd = spacingAdd;
         this.mIncludePad = inclidePad;
         this.mPages = new ArrayList<CharSequence>();
+        this.mReverse = reverse;
 
-        layout();
+        if (mReverse)
+            layout();
+
+        else reverseLayout();
     }
 
     private void layout() {
@@ -55,6 +63,38 @@ public class Pagination {
                 return;
             }
         }
+        Log.v("abc", "abc");
+    }
+
+    private void reverseLayout() {
+        final StaticLayout layout = new StaticLayout(mText, mPaint, mWidth, Layout.Alignment.ALIGN_NORMAL, mSpacingMult, mSpacingAdd, mIncludePad);
+
+        final int lines = layout.getLineCount();
+        final CharSequence text = layout.getText();
+        int startOffset = 0;
+        int height = mHeight;
+        String subSeq;
+        CharSequence reverse;
+        for (int i = 0; i < lines; i++) {
+            if (height < layout.getLineBottom(i)) {
+                // When the layout height has been exceeded
+                subSeq = text.subSequence(startOffset, layout.getLineStart(i)).toString();
+                reverse = new StringBuilder(subSeq).reverse().toString();
+                addPage(reverse);
+                startOffset = layout.getLineStart(i);
+                height = layout.getLineTop(i) + mHeight;
+            }
+
+            if (i == lines - 1) {
+                // Put the rest of the text into the last page
+                subSeq = text.subSequence(startOffset, layout.getLineEnd(i)).toString();
+                reverse = new StringBuilder(subSeq).reverse().toString();
+                addPage(reverse);
+                return;
+            }
+        }
+        // Collections.reverse(mPages);
+        Log.v("abc","abc");
     }
 
     private void addPage(CharSequence text) {
--- app/src/main/java/com/example/onlinetyari/storyreader/StoryActivity.java
+++ app/src/main/java/com/example/onlinetyari/storyreader/StoryActivity.java
@@ -2,12 +2,11 @@ package com.example.onlinetyari.storyreader;
 
 import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.text.Html;
+import android.text.Layout;
 import android.text.Spanned;
+import android.text.StaticLayout;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.Menu;
@@ -17,34 +16,33 @@ import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import java.util.Collections;
+
 public class StoryActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
 
     public TextView textView;
 
     public Pagination mPagination;
+    public Pagination prevPagination;
+    public Pagination nextPagination;
     public CharSequence mText;
+    public CharSequence prevText = "";
+    public CharSequence currentText;
+    public CharSequence nextText;
     public int mCurrentIndex = 0;
     public Button backButton;
     public Button forwardButton;
     public SeekBar seekBar;
+    public int i;
+    public int oldProgress = 24;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_story);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setContentView(R.layout.content_story);
+        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        assert fab != null;
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
+*/
         textView = (TextView) findViewById(R.id.textView);
 
         assert textView != null;
@@ -57,7 +55,6 @@ public class StoryActivity extends AppCompatActivity implements SeekBar.OnSeekBa
 
         Spanned htmlString = Html.fromHtml(book_content);
         mText = TextUtils.concat(htmlString);
-
         textView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
             @Override
             public void onGlobalLayout() {
@@ -67,16 +64,48 @@ public class StoryActivity extends AppCompatActivity implements SeekBar.OnSeekBa
                 } else {
                     textView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
                 }
+
                 mPagination = new Pagination(mText,
                         textView.getWidth(),
                         textView.getHeight(),
                         textView.getPaint(),
                         textView.getLineSpacingMultiplier(),
                         textView.getLineSpacingExtra(),
-                        textView.getIncludeFontPadding());
+                        textView.getIncludeFontPadding(),
+                        true);
+
+                prevPagination = new Pagination(prevText,
+                        textView.getWidth(),
+                        textView.getHeight(),
+                        textView.getPaint(),
+                        textView.getLineSpacingMultiplier(),
+                        textView.getLineSpacingExtra(),
+                        textView.getIncludeFontPadding(),
+                        true);
+
+                currentText = mPagination.mPages.get(mCurrentIndex);
+
+                StringBuilder stringBuilder = new StringBuilder();
+                for (i = 1; i < mPagination.mPages.size(); i++) {
+                    stringBuilder.append(mPagination.mPages.get(i));
+                }
+
+                nextText = stringBuilder.toString();
+
+                nextPagination = new Pagination(nextText,
+                        textView.getWidth(),
+                        textView.getHeight(),
+                        textView.getPaint(),
+                        textView.getLineSpacingMultiplier(),
+                        textView.getLineSpacingExtra(),
+                        textView.getIncludeFontPadding(),
+                        true);
+
                 update();
             }
         });
+
+        getCurrentString();
 
         backButton = (Button) findViewById(R.id.back_btn);
         forwardButton = (Button) findViewById(R.id.forward_btn);
@@ -101,16 +130,10 @@ public class StoryActivity extends AppCompatActivity implements SeekBar.OnSeekBa
             }
         });
 
-        seekBar = (SeekBar) findViewById(R.id.seekbar);
+        seekBar = (SeekBar) findViewById(R.id.seek_bar);
         assert seekBar != null;
-        seekBar.setProgress((int) textView.getTextSize());
+        seekBar.setProgress(oldProgress);
         seekBar.setOnSeekBarChangeListener(this);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                seekBar.setVisibility(View.VISIBLE);
-            }
-        });
     }
 
     @Override
@@ -136,8 +159,192 @@ public class StoryActivity extends AppCompatActivity implements SeekBar.OnSeekBa
     }
 
     @Override
-    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+    public void onProgressChanged(SeekBar seekBar, final int progress, boolean fromUser) {
         textView.setTextSize(progress);
+
+        textView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+
+                if (progress > oldProgress) {
+                    // Removing layout listener to avoid multiple calls
+                    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+                        textView.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                    } else {
+                        textView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                    }
+
+                    if (mCurrentIndex == mPagination.size())
+                        mCurrentIndex--;
+                    currentText = mPagination.mPages.get(mCurrentIndex);
+
+                    StringBuilder stringBuilder;
+                    stringBuilder = new StringBuilder();
+
+                    for (i = 0; i < mCurrentIndex; i++) {
+                        stringBuilder.append(mPagination.get(i));
+                    }
+
+                    prevText = stringBuilder.toString();
+
+                    stringBuilder = new StringBuilder();
+                    for (i = mCurrentIndex + 1; i < mPagination.mPages.size(); i++) {
+                        stringBuilder.append(mPagination.mPages.get(i));
+                    }
+
+                    nextText = stringBuilder.toString();
+
+                    mPagination = new Pagination(mText,
+                            textView.getWidth(),
+                            textView.getHeight(),
+                            textView.getPaint(),
+                            textView.getLineSpacingMultiplier(),
+                            textView.getLineSpacingExtra(),
+                            textView.getIncludeFontPadding(),
+                            true);
+                    // update();
+
+                    CharSequence reverse = new StringBuilder(prevText).reverse().toString();
+
+                    prevPagination = new Pagination(reverse,
+                            textView.getWidth(),
+                            textView.getHeight(),
+                            textView.getPaint(),
+                            textView.getLineSpacingMultiplier(),
+                            textView.getLineSpacingExtra(),
+                            textView.getIncludeFontPadding(),
+                            false);
+
+                    Collections.reverse(prevPagination.mPages);
+
+                    CharSequence newCurrentText = getCurrentString();
+
+                    int startIndex = newCurrentText.length();
+
+                    stringBuilder = new StringBuilder();
+                    stringBuilder.append(currentText.subSequence(startIndex, currentText.length()));
+                    stringBuilder.append(nextText);
+
+                    nextText = stringBuilder.toString();
+
+                    nextPagination = new Pagination(nextText,
+                            textView.getWidth(),
+                            textView.getHeight(),
+                            textView.getPaint(),
+                            textView.getLineSpacingMultiplier(),
+                            textView.getLineSpacingExtra(),
+                            textView.getIncludeFontPadding(),
+                            true);
+
+                    mPagination.mPages.clear();
+
+                    if (!prevText.equals(""))
+                        mPagination.mPages.addAll(prevPagination.mPages);
+
+                    mPagination.mPages.add(newCurrentText);
+
+                    if (!nextText.equals(""))
+                        mPagination.mPages.addAll(nextPagination.mPages);
+                    mCurrentIndex = prevPagination.mPages.size();
+                    update();
+                }
+
+                if (progress < oldProgress) {
+                    // Removing layout listener to avoid multiple calls
+                    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+                        textView.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                    } else {
+                        textView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                    }
+
+                    StringBuilder stringBuilder;
+                    CharSequence nextPageText = "";
+
+                    if (mCurrentIndex < mPagination.size() - 1)
+                        nextPageText = mPagination.mPages.get(mCurrentIndex + 1);
+
+                    stringBuilder = new StringBuilder();
+                    stringBuilder.append(mPagination.mPages.get(mCurrentIndex));
+                    if (mCurrentIndex < mPagination.size() - 1)
+                        stringBuilder.append(mPagination.mPages.get(mCurrentIndex + 1));
+                    currentText = stringBuilder.toString();
+
+                    stringBuilder = new StringBuilder();
+
+                    for (i = 0; i < mCurrentIndex; i++) {
+                        stringBuilder.append(mPagination.get(i));
+                    }
+
+                    prevText = stringBuilder.toString();
+
+                    stringBuilder = new StringBuilder();
+                    for (i = mCurrentIndex + 1; i < mPagination.mPages.size(); i++) {
+                        stringBuilder.append(mPagination.mPages.get(i));
+                    }
+
+                    nextText = stringBuilder.toString();
+
+                    mPagination = new Pagination(mText,
+                            textView.getWidth(),
+                            textView.getHeight(),
+                            textView.getPaint(),
+                            textView.getLineSpacingMultiplier(),
+                            textView.getLineSpacingExtra(),
+                            textView.getIncludeFontPadding(),
+                            true);
+                    // update();
+
+                    CharSequence reverse = new StringBuilder(prevText).reverse().toString();
+
+                    prevPagination = new Pagination(reverse,
+                            textView.getWidth(),
+                            textView.getHeight(),
+                            textView.getPaint(),
+                            textView.getLineSpacingMultiplier(),
+                            textView.getLineSpacingExtra(),
+                            textView.getIncludeFontPadding(),
+                            false);
+
+                    Collections.reverse(prevPagination.mPages);
+
+                    CharSequence newCurrentText = fitString(currentText);
+
+                    int startIndex = currentText.length() - newCurrentText.length();
+                    stringBuilder = new StringBuilder();
+
+                    if (nextPageText.equals("")) {
+                        startIndex = nextPageText.length() - startIndex;
+                        stringBuilder.append(nextText.subSequence(startIndex, nextText.length()));
+                    }
+                    nextText = stringBuilder.toString();
+
+                    nextPagination = new Pagination(nextText,
+                            textView.getWidth(),
+                            textView.getHeight(),
+                            textView.getPaint(),
+                            textView.getLineSpacingMultiplier(),
+                            textView.getLineSpacingExtra(),
+                            textView.getIncludeFontPadding(),
+                            true);
+
+                    mPagination.mPages.clear();
+
+                    if (!prevText.equals(""))
+                        mPagination.mPages.addAll(prevPagination.mPages);
+
+                    mPagination.mPages.add(newCurrentText);
+
+                    if (!nextText.equals(""))
+                        mPagination.mPages.addAll(nextPagination.mPages);
+
+                    mCurrentIndex = prevPagination.mPages.size();
+
+                    update();
+                }
+
+                oldProgress = progress;
+            }
+        });
     }
 
     @Override
@@ -147,12 +354,61 @@ public class StoryActivity extends AppCompatActivity implements SeekBar.OnSeekBa
 
     @Override
     public void onStopTrackingTouch(SeekBar seekBar) {
-        seekBar.setVisibility(View.GONE);
+
     }
 
     private void update() {
         final CharSequence text = mPagination.get(mCurrentIndex);
         if (text != null) textView.setText(text);
+    }
+
+    private CharSequence getCurrentString() {
+        final StaticLayout layout = new StaticLayout(textView.getText(), textView.getPaint(), textView.getWidth(), Layout.Alignment.ALIGN_NORMAL, textView.getLineSpacingMultiplier(), textView.getLineSpacingExtra(), textView.getIncludeFontPadding());
+
+        final int lines = layout.getLineCount();
+        final CharSequence text = layout.getText();
+        int startOffset = 0;
+        int height = textView.getHeight();
+
+        for (int i = 0; i < lines; i++) {
+            if (height < layout.getLineBottom(i)) {
+                // When the layout height has been exceeded
+                return text.subSequence(startOffset, layout.getLineStart(i));
+            }
+
+            if (i == lines - 1) {
+                // Put the rest of the text into the last page
+                return text.subSequence(startOffset, layout.getLineEnd(i));
+            }
+        }
+
+        return "";
+    }
+
+    private CharSequence fitString(CharSequence text) {
+        final StaticLayout layout = new StaticLayout(text, textView.getPaint(), textView.getWidth(), Layout.Alignment.ALIGN_NORMAL, textView.getLineSpacingMultiplier(), textView.getLineSpacingExtra(), textView.getIncludeFontPadding());
+
+        final int lines = layout.getLineCount();
+        int startOffset = 0;
+        int height = textView.getHeight();
+
+        for (int i = 0; i < lines; i++) {
+            if (height < layout.getLineBottom(i)) {
+                // When the layout height has been exceeded
+                return text.subSequence(startOffset, layout.getLineStart(i));
+            }
+
+            if (i == lines - 1) {
+                // Put the rest of the text into the last page
+                return text.subSequence(startOffset, layout.getLineEnd(i));
+            }
+        }
+
+        return "";
+    }
+
+    public void resetPages() {
+
     }
 
 }
